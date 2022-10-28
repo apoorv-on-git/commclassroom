04011f5e25c3c2f3be40d94f0761a775e98508ad
Apoorv Goyal <apoorvelous@gmail.com>
2022-10-28 23:12:34 +0530

    Updates

---
+++ precache-manifest.3f7cf35bd4261dd98528809f2015174a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9dbe62cc7fc122cfd37",
+    "url": "/commclassroom/static/css/main.d8ced151.chunk.css"
+  },
+  {
+    "revision": "a9dbe62cc7fc122cfd37",
+    "url": "/commclassroom/static/js/main.a9dbe62c.chunk.js"
+  },
+  {
+    "revision": "11d2c66dc94cc166b8ad",
+    "url": "/commclassroom/static/js/1.11d2c66d.chunk.js"
+  },
+  {
+    "revision": "3cf512bcf3ae3661f270",
+    "url": "/commclassroom/static/js/runtime~main.3cf512bc.js"
+  },
+  {
+    "revision": "5171b091e495aa90ddf820606ac58cfe",
+    "url": "/commclassroom/static/media/DSA.5171b091.png"
+  },
+  {
+    "revision": "17db66ecfb373f1d404c5e3c0a881aa5",
+    "url": "/commclassroom/static/media/Github.17db66ec.png"
+  },
+  {
+    "revision": "44ac8190d89087b65d70611cdd342055",
+    "url": "/commclassroom/static/media/MachineLearning.44ac8190.png"
+  },
+  {
+    "revision": "8b7d99f3b4c1f9f6e5f044360489e7b9",
+    "url": "/commclassroom/static/media/SystemDesign.8b7d99f3.png"
+  },
+  {
+    "revision": "915b554f22daaa014b39de48177d14c2",
+    "url": "/commclassroom/static/media/FullStack.915b554f.png"
+  },
+  {
+    "revision": "9326999a7479a287368edf0de50c7460",
+    "url": "/commclassroom/static/media/DevOps.9326999a.png"
+  },
+  {
+    "revision": "3bffb4fd6ee98ed4b8c03ce87f9297f4",
+    "url": "/commclassroom/static/media/Python.3bffb4fd.png"
+  },
+  {
+    "revision": "452dbcc47a41d5c940d98f7066fbc075",
+    "url": "/commclassroom/static/media/podcast.452dbcc4.png"
+  },
+  {
+    "revision": "5d4ce39341050ae34780318b43adadea",
+    "url": "/commclassroom/static/media/Founder.5d4ce393.png"
+  },
+  {
+    "revision": "81f5b39d75f7a300202766f9cc0de357",
+    "url": "/commclassroom/static/media/about-us.81f5b39d.png"
+  },
+  {
+    "revision": "ac374f37de34d7f4b664b28162a77729",
+    "url": "/commclassroom/static/media/course.ac374f37.png"
+  },
+  {
+    "revision": "60b5eb7fb84eb5bcbcf2b82ee8346026",
+    "url": "/commclassroom/static/media/event.60b5eb7f.png"
+  },
+  {
+    "revision": "8dde36c62dd0e2d1844adffcfc9a5aa9",
+    "url": "/commclassroom/static/media/mentor.8dde36c6.png"
+  },
+  {
+    "revision": "77fed3530ec954df60bf50b55ca07a5e",
+    "url": "/commclassroom/static/media/blog.77fed353.png"
+  },
+  {
+    "revision": "d30f046e7d2c087c15a41c27b957f7e4",
+    "url": "/commclassroom/static/media/events_sema.d30f046e.png"
+  },
+  {
+    "revision": "00a172bee655b4e35e130d1ea36715b3",
+    "url": "/commclassroom/static/media/events_hashnode.00a172be.png"
+  },
+  {
+    "revision": "5c6a47a980c966acd0ced48218e03c27",
+    "url": "/commclassroom/static/media/events_coding.5c6a47a9.png"
+  },
+  {
+    "revision": "c17b58377c1ccfee3e044f5592477da5",
+    "url": "/commclassroom/static/media/events_kubescape.c17b5837.png"
+  },
+  {
+    "revision": "735a6ff3c082ab8e50614bbd2de8a833",
+    "url": "/commclassroom/index.html"
+  }
+];
